74956e1d5b2c2e3ef2e08221b4755814ded4adc3
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2020-10-08 11:21:18 +0300

    update

--- frontend/src/services/ordersService.js
+++ frontend/src/services/ordersService.js
@@ -4,7 +4,16 @@ const baseUrl = 'http://localhost:3001/api/orders';
 
 const getUserOrders = () => {
   if (token) {
-      const res = 
+    const options = {
+      headers: { Authorization: token },
+    };
+
+    const res = axios
+      .get(baseUrl, options)
+      .then((response) => response.data)
+      .catch((error) => error.response.data);
+
+    return res;
   }
   return null;
 };
